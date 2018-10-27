ebcb054d774cf58b55147f83107e17bfc05807dc
liqijava <17839230891@163.com>
2018-10-27 11:40:48 +0800

    李琦

---
+++ src/main/java/com/fh/shop/common/students.java
@@ -0,0 +1,22 @@
+package com.fh.shop.common;
+
+public class students {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
